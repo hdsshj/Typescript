080c325c79e91de4a85f82c5bb227e10bb2ab51d
Wooseok Han <wswj9608@naver.com>
2022-01-15 02:56:24 +0900

    기본 타입 정리

---
+++ 기본 타입 정리/primitive_types.js
@@ -0,0 +1,10 @@
+// 변수에 타입지정
+var userName = "kim";
+var age = 50;
+var state = undefined;
+// 배열, 객체에 타입 지정
+var memberList = ["kim", "park"];
+var memberObject = { name: "kim", age: 19 };
+// 타입스크립트는 대부분 타입지정을 자동으로 한다.
+var userName2 = "kim";
+var age2 = 50;
--- 기본 타입 정리/primitive_types.ts
+++ 기본 타입 정리/primitive_types.ts
@@ -1,1 +1,12 @@
- 
+// 변수에 타입지정
+let userName: string = "kim";
+let age: number = 50;
+let state: undefined = undefined;
+
+// 배열, 객체에 타입 지정
+let memberList: string[] = ["kim", "park"];
+let memberObject: { name: string; age: number } = { name: "kim", age: 19 };
+
+// 타입스크립트는 대부분 타입지정을 자동으로 한다.
+let userName2 = "kim";
+let age2 = 50;
