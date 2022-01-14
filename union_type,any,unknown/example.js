a367078493593271915d7001a53da4068620edc2
Wooseok Han <wswj9608@naver.com>
2022-01-15 03:30:41 +0900

    union type, any, unknown

---
+++ union_type,any,unknown/example.js
@@ -0,0 +1,12 @@
+// 타입 지정 해보기
+var user = "kim";
+var age1 = undefined;
+var married = false;
+var 철수 = [user, age1, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
---
+++ union_type,any,unknown/union_type, any, unknown.ts
@@ -0,0 +1,31 @@
+// 타입을 2개 이상 합친 새로운 타입 (Union Type)
+let member: number | string = 1;
+let members: (number | string)[] = [1, "2", 3];
+let object: { a: string | number } = { a: 1 };
+
+// 간단한 연산도 타입이 맞아야 한다.
+// string + 1 (허용)
+// number + 1 (허용)
+// string | number + 1 (허용 안됨)
+// number type, string type이 아닌 'number | string' 이라는 새로운 타입이기 떄문
+
+let var1: string;
+let var2: number;
+let var3: number | string;
+
+var1 + 1;
+var2 + 1;
+// var3 + 1 은 에러 검출
+
+// 타입실드 해제 문법
+// 일반 js 변수처럼 사용
+let nameAny: any;
+nameAny = 123;
+
+// any와 유사하지만 타입실드를 해제하지 않음
+let nameUnknown: unknown;
+nameUnknown = 123;
+
+// nameAny는 에러 검출 안됨
+// nameUnknown은 에러 검출 됨
+let newVar: string = nameAny;
